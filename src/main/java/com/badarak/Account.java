c7a8a943254c0b1c973ba16f4e7881455bccf1a3
Aliou <badarak20@gmail.com>
2022-10-29 13:44:24 +0200

    [us-2] - make make a withdrawal from an account

--- src/main/java/com/badarak/Account.java
+++ src/main/java/com/badarak/Account.java
@@ -3,6 +3,7 @@ package com.badarak;
 public class Account {
 
     private double balance;
+    private final double OVERDRAFT_LIMIT = 500;
 
     public Account() {
     }
@@ -14,6 +15,14 @@ public class Account {
     public void deposit(double amount) {
         this.balance = this.balance + amount;
 
+    }
+
+    public void withdrawal(double amount) {
+        if (balance - amount < 0) {
+           throw new RuntimeException("Withdrawal will result in an overdraft");
+        }
+
+        balance -= amount;
     }
 
     public double getBalance() {
