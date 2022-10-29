dc431fbb6df613204bb9cf97aecfbd68c461eec1
Aliou <badarak20@gmail.com>
2022-10-29 13:44:24 +0200

    [us-2] - make make a withdrawal from an account

--- src/test/java/com/badarak/AccountTest.java
+++ src/test/java/com/badarak/AccountTest.java
@@ -3,6 +3,8 @@ package com.badarak;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class AccountTest {
 
@@ -12,13 +14,24 @@ public class AccountTest {
         Account account1 = new Account(10);
         Account account2 = new Account();
 
-
         account1.deposit(20);
         account2.deposit(20);
 
-        //
         assertEquals(30, account1.getBalance());
         assertEquals(20, account2.getBalance());
+    }
 
+
+    @Test
+    public void test_withdrawal_without_overdraft() {
+        Account account = new Account(10);
+        account.withdrawal(5);
+        assertEquals(5, account.getBalance());
+    }
+
+    @Test
+    public void test_withdrawal_resulting_in_overdraft() {
+        Account account = new Account(10);
+        assertThrows(RuntimeException.class, () -> account.withdrawal(15));
     }
 }
