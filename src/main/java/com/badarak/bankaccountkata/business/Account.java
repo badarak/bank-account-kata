2204ff30644ce9a5cd952cb0fe2c6ceaa5695b14
Aliou <badarak20@gmail.com>
2022-11-05 20:35:40 +0100

    make date of operation as parameter of methods deposit or withdraw

--- src/main/java/com/badarak/bankaccountkata/business/Account.java
+++ src/main/java/com/badarak/bankaccountkata/business/Account.java
@@ -1,7 +1,6 @@
 package com.badarak.bankaccountkata.business;
 
 import com.badarak.bankaccountkata.exception.UnsufficientBalanceException;
-
 import java.time.LocalDateTime;
 
 public class Account {
@@ -9,29 +8,30 @@ public class Account {
     private final Amount balance;
     private final Statement statement;
 
+
     public Account(AccountNumber accountId, Amount balance, Statement statement) {
         this.accountId = accountId;
         this.balance = balance;
         this.statement = statement;
     }
 
-    public Account deposit(Amount amount) {
+    public Account deposit(Amount amount, LocalDateTime date) {
         Amount newBalance = balance.add(amount);
-        return treatOperation(TransactionType.DEPOSIT, amount, newBalance);
+        return treatOperation(TransactionType.DEPOSIT, amount, newBalance, date);
     }
 
-    private Account treatOperation(TransactionType type, Amount amount, Amount newBalance) {
-        Statement newStatement = this.statement.addNewTransaction(new Transaction(LocalDateTime.now(), type, amount, newBalance));
+    private Account treatOperation(TransactionType type, Amount amount, Amount newBalance, LocalDateTime date) {
+        Statement newStatement = this.statement.addNewTransaction(new Transaction(date, type, amount, newBalance));
         return new Account(accountId, newBalance, newStatement);
     }
 
-    public Account withdraw(Amount amount) {
+    public Account withdraw(Amount amount, LocalDateTime date) {
         if (amount.isGreaterThan(balance)) {
            throw new UnsufficientBalanceException("Insufficient balance for this withdrawal.");
         }
 
         Amount newBalance = balance.minus(amount);
-        return treatOperation(TransactionType.WITHDRAWAL, amount, newBalance);
+        return treatOperation(TransactionType.WITHDRAWAL, amount, newBalance, date);
     }
 
     public Amount getBalance() {
